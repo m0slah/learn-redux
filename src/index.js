e1eb59efc136c697d83fd825c85cfb0fcd7571ee
m0slah <muhammedslahqadir@gmail.com>
2024-07-12 17:13:34 +0300

    action

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createStore } from "redux";
+
+//STORE => GLOBAL STATE
+
+//ACTION INCREMENT
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+// REDUCER
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+store.subscribe(() => console.log(store.getState()));
+
+// DISPATH
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -8,4 +43,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
