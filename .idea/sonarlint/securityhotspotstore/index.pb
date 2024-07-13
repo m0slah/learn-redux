f0511e98fcf0b3279356b6d7ed1a581392c96fa4
m0slah <muhammedslahqadir@gmail.com>
2024-07-13 15:38:26 +0300

    use counter state in app

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+G
+src/reducers/counter.js,f\d\fdaf4575f1e67efc26603fe21c4b33dabd3fdb47
--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
 import "./App.css";
+import { useSelector } from "react-redux";
 
 function App() {
-  return <div className="App">react </div>;
+  const [counter, setCounter] = useState(useSelector((state) => state.counter));
+
+  const increment = () => {
+    setCounter(counter + 1);
+  };
+
+  return (
+    <>
+      <div className="App">counter is: {counter}</div>
+      <button onClick={increment}>increment</button>
+    </>
+  );
 }
 
 export default App;
